82a6e67c23da296301c58b69cc120899e65afd9a
蔺绍宏 <shaohonglin0411@gmail.com>
2019-07-22 19:12:33 +0800

    modify Retry interface

--- common/boot/src/main/java/com/github/sun/foundation/boot/utility/Retry.java
+++ common/boot/src/main/java/com/github/sun/foundation/boot/utility/Retry.java
@@ -6,18 +6,21 @@ import java.util.concurrent.Callable;
 
 @UtilityClass
 public class Retry {
+  public <T> T execute(int maxCount, Callable<T> func) throws Exception {
+    return execute(maxCount, 0, func);
+  }
+
   public <T> T execute(int maxCount, long delayInMs, Callable<T> func) throws Exception {
     int counter = 1;
     maxCount = maxCount <= 0 ? 1 : maxCount;
     for (; ; ) {
+      Thread.sleep(delayInMs);
       try {
         return func.call();
       } catch (Exception ex) {
         counter++;
         if (counter > maxCount) {
           throw ex;
-        } else {
-          Thread.sleep(delayInMs);
         }
       }
     }
