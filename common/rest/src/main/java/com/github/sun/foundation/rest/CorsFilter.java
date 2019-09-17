d88ac19f1390779084270ca7fa0c4d684138ce97
蔺绍宏 <shaohonglin0411@gmail.com>
2019-09-17 10:46:38 +0800

    add cors filter

---
+++ common/rest/src/main/java/com/github/sun/foundation/rest/CorsFilter.java
@@ -0,0 +1,18 @@
+package com.github.sun.foundation.rest;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+  @Override
+  public void filter(ContainerRequestContext request, ContainerResponseContext response) {
+    response.getHeaders().add("Access-Control-Allow-Origin", "*");
+    response.getHeaders().add("Access-Control-Allow-Headers", "*");
+    response.getHeaders().add("Access-Control-Expose-Headers", "*");
+    response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+    response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+  }
+}
