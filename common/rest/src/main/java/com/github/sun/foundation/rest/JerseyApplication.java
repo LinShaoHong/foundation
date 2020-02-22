559d595c34e140a806b74da885b665540d374619
LinShaoHong <lins@LinShaoHong-MacBook.local>
2020-02-22 17:47:40 +0800

    support swagger2

--- common/rest/src/main/java/com/github/sun/foundation/rest/JerseyApplication.java
+++ common/rest/src/main/java/com/github/sun/foundation/rest/JerseyApplication.java
@@ -3,17 +3,35 @@ package com.github.sun.foundation.rest;
 import com.github.sun.foundation.boot.Bootstrap;
 import com.github.sun.foundation.boot.Scanner;
 import com.github.sun.foundation.boot.utility.Configurators;
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.config.SwaggerConfigLocator;
+import io.swagger.jaxrs.config.SwaggerContextService;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
+import io.swagger.models.Info;
 import lombok.extern.slf4j.Slf4j;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.media.sse.SseFeature;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.Path;
 import javax.ws.rs.ext.Provider;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
+@ApplicationPath("/api")
 public abstract class JerseyApplication<A> extends ResourceConfig {
   @SuppressWarnings("unchecked")
   public JerseyApplication(ApplicationContext context) {
@@ -35,6 +53,37 @@ public abstract class JerseyApplication<A> extends ResourceConfig {
     HashSet<Class<?>> set = new HashSet<>();
     set.addAll(providers);
     set.addAll(resources);
+    // swagger
+    set.add(ApiListingResource.class);
+    set.add(SwaggerSerializers.class);
+    // multipart
+    set.add(MultiPartFeature.class);
+    // sse
+    set.add(SseFeature.class);
     registerClasses(set);
+  }
+
+  @Value("${spring.application.name}")
+  private String appName;
+
+  @Bean
+  public Docket docket() {
+    BeanConfig swagger = new BeanConfig();
+    swagger.setBasePath("/api");
+    swagger.setInfo(new Info().title(appName + " API"));
+    SwaggerConfigLocator.getInstance().putConfig(SwaggerContextService.CONFIG_ID_DEFAULT, swagger);
+    return new Docket(DocumentationType.SWAGGER_2).select().build();
+  }
+
+  @Bean
+  @Primary
+  public SwaggerResourcesProvider swaggerResource() {
+    return () -> {
+      SwaggerResource resource = new SwaggerResource();
+      resource.setLocation("/api/swagger.json");
+      resource.setSwaggerVersion("2.0");
+      resource.setName("Jersey");
+      return Collections.singletonList(resource);
+    };
   }
 }
