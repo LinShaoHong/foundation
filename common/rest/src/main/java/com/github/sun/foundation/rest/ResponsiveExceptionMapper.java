3c69d823829e0b99c65e1f388d4eb200b947cff6
LinShaoHong <lins@LinShaoHong-MacBook.local>
2020-03-27 17:18:05 +0800

    fux

--- common/rest/src/main/java/com/github/sun/foundation/rest/ResponsiveExceptionMapper.java
+++ common/rest/src/main/java/com/github/sun/foundation/rest/ResponsiveExceptionMapper.java
@@ -1,10 +1,13 @@
 package com.github.sun.foundation.rest;
 
 import com.github.sun.foundation.boot.exception.ResponsiveException;
+import com.github.sun.foundation.boot.utility.Iterators;
 import lombok.extern.slf4j.Slf4j;
+import org.glassfish.hk2.api.MultiException;
 
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import java.util.List;
 
 @Slf4j
 @Provider
@@ -14,6 +17,26 @@ public class ResponsiveExceptionMapper implements ExceptionMapper<ResponsiveExce
   @Override
   public javax.ws.rs.core.Response toResponse(ResponsiveException exception) {
     return helper.toResponse(exception);
+  }
+
+  @Provider
+  public static class MultiExceptionMapper extends AbstractResource implements ExceptionMapper<MultiException> {
+    private Helper helper = new Helper();
+
+    @Override
+    public javax.ws.rs.core.Response toResponse(MultiException ex) {
+      List<Throwable> errors = ex.getErrors();
+      Throwable throwable = errors.stream().filter(v -> (v instanceof ResponsiveException)).findFirst().orElse(null);
+      if (throwable != null) {
+        return helper.toResponse((ResponsiveException) throwable);
+      }
+      javax.ws.rs.core.Response.Status status = javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
+      String message = Iterators.mkString(errors, "\n", Throwable::getMessage);
+      return javax.ws.rs.core.Response.status(status)
+        .entity(responseOf(status.getStatusCode(), message))
+        .type("application/json;charset=utf8")
+        .build();
+    }
   }
 
   private static class Helper extends AbstractResource {
