57bfafcbc1418d9df0ebdeac4f73b9624187128b
LinShaoHong <shaohonglin0411@gmail.com>
2024-01-25 09:53:19 +0800

    fix

--- common/rest/src/main/java/com/github/sun/foundation/rest/JerseyClient.java
+++ common/rest/src/main/java/com/github/sun/foundation/rest/JerseyClient.java
@@ -1,6 +1,8 @@
 package com.github.sun.foundation.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.sun.foundation.boot.InjectionProvider;
+import com.github.sun.foundation.boot.Injector;
 import com.github.sun.foundation.boot.utility.JSON;
 import com.github.sun.foundation.boot.utility.SSL;
 import org.glassfish.jersey.client.ClientConfig;
@@ -10,9 +12,13 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.ext.ContextResolver;
 
 public class JerseyClient {
-  private static Client client;
+  private static volatile Client client;
 
-  public static Client build() {
+  public static Client of() {
+    return Injector.getInstance(Client.class);
+  }
+
+  private static Client build() {
     if (client == null) {
       synchronized (JerseyClient.class) {
         if (client == null) {
@@ -27,5 +33,12 @@ public class JerseyClient {
       }
     }
     return client;
+  }
+
+  public static class Provider implements InjectionProvider {
+    @Override
+    public void config(Binder binder) {
+      binder.named("rest-client").bind(build());
+    }
   }
 }
