50f5a8dfd069dfe8c36b6f0a30f4c136c4be314a
LinShaoHong <shaohonglin0411@gmail.com>
2024-02-06 19:03:15 +0800

    fix

--- common/boot/src/main/java/com/github/sun/foundation/boot/utility/IPs.java
+++ common/boot/src/main/java/com/github/sun/foundation/boot/utility/IPs.java
@@ -1,29 +1,31 @@
 package com.github.sun.foundation.boot.utility;
 
 import lombok.experimental.UtilityClass;
+import org.springframework.util.StringUtils;
 
-import javax.ws.rs.container.ContainerRequestContext;
+import javax.servlet.http.HttpServletRequest;
 
 @UtilityClass
 public class IPs {
-  public String getRemoteIP(ContainerRequestContext ctx) {
-    String ip;
-    try {
-      ip = ctx.getHeaderString("x-forwarded-for");
-      if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-        ip = ctx.getHeaderString("Proxy-Client-IP");
-      }
-      if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-        ip = ctx.getHeaderString("WL-Proxy-Client-IP");
-      }
-      // 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
-      if (ip != null && ip.length() > 15) { // "***.***.***.***".length()
-        if (ip.indexOf(",") > 0) {
-          ip = ip.substring(0, ip.indexOf(","));
-        }
-      }
-    } catch (Exception e) {
-      ip = "";
+  public String getRemoteIP(HttpServletRequest request) {
+    String ip = request.getHeader("X-Real-IP");
+    if (!StringUtils.hasText(ip) || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getHeader("X-Forwarded-For");
+    }
+    if (!StringUtils.hasText(ip) || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getHeader("Proxy-Client-IP");
+    }
+    if (!StringUtils.hasText(ip) || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getHeader("WL-Proxy-Client-IP");
+    }
+    if (!StringUtils.hasText(ip) || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getHeader("HTTP_CLIENT_IP");
+    }
+    if (!StringUtils.hasText(ip) || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+    }
+    if (!StringUtils.hasText(ip) || "unknown".equalsIgnoreCase(ip)) {
+      ip = request.getRemoteAddr();
     }
     return ip;
   }
