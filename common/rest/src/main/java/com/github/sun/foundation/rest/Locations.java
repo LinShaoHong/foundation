ea680c5e4ff5f1c62b5686d4ff51bbf3c28d2fd8
LinShaoHong <shaohonglin0411@gmail.com>
2024-02-11 19:12:28 +0800

    fix

--- common/rest/src/main/java/com/github/sun/foundation/rest/Locations.java
+++ common/rest/src/main/java/com/github/sun/foundation/rest/Locations.java
@@ -11,17 +11,18 @@ public class Locations {
     if (ip != null && !ip.isEmpty()) {
       try {
         JsonNode node = JerseyClient.of()
-          .property(ClientProperties.READ_TIMEOUT, 500)
-          .property(ClientProperties.CONNECT_TIMEOUT, 500)
-          .target("http://ip-api.com/json?ip=" + ip + "&lang=zh-CN&fields=status,country,city")
+          .property(ClientProperties.READ_TIMEOUT, 3000)
+          .property(ClientProperties.CONNECT_TIMEOUT, 3000)
+          .target("http://ip-api.com/json/" + ip + "?lang=zh-CN&fields=status,country,city,regionName")
           .request()
           .get()
           .readEntity(JsonNode.class);
         String status = node.get("status").asText();
         if ("success".equals(status)) {
           String city = node.get("city").asText();
+          String regionName = node.get("regionName").asText();
           String country = node.get("country").asText();
-          location = country + ":" + city;
+          location = country + ":" + regionName + ":" + city;
         }
       } catch (Throwable ex) {
         // do nothing
